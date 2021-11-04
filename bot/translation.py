0bdd7f956d86ea6af214dcbf701c5b5badbd6e7b
myrplix <93067102+myrplix@users.noreply.github.com>
2021-11-04 16:57:30 +0800

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@amirul_minie
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Myrplix Filter Bot</code>
